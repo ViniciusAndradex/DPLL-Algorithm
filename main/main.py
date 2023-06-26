0fe4856c29f6264c7c30760ce0d2a50ea423ccff
ViniciusAndradeX <viniciusandradeprog@gmail.com>
2023-06-26 20:30:55 -0300

    Projeto concluíd

--- main/main.py
+++ main/main.py
@@ -1,10 +1,12 @@
 from sat.dpll_class import Dpll
+from sat.dpll import Dpll as dP
 from utils.cnf_to_list_and_set import cnf_to_list
 from utils.input_generator import get_disciplines, to_model, atoms_map
 from utils.literal_converter import LiteralConverter
 from utils.clausal_converter import ClausalFormConverter
 
 dppl = Dpll()
+dppl_teste = dP()
 
 clauses = cnf_to_list("inputs/teste_4.cnf")
 
@@ -15,4 +17,5 @@ dict_map = atoms_map(len(disciplines), 10)
 converter = LiteralConverter(dict_map)
 cnf_model = converter.to_clauses_of_int(ClausalFormConverter.convert(to_model(disciplines, 10)))
 
-dppl.dpll(cnf_model, converter)
+dppl.dpll(clauses, set())
+dppl_teste.dpll(cnf_model, set())
