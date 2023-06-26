b0f213ea6065764ecec1c2370474e2781a9c4187
ViniciusAndradeX <viniciusandradeprog@gmail.com>
2023-06-25 21:13:05 -0300

    Functions add.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from dpll_class import Dpll
-
-clauses = [{1, 2}, {2, -1}, {2}, {2, 3}]
+from sat.dpll_class import Dpll
+from utils.cnf_to_list_and_set import cnf_to_list
 
 dppl = Dpll()
+
+clauses = cnf_to_list("inputs/teste_1.cnf")
+
+print(clauses)
 
 teste = dppl.dpll(clauses, set())
 print(teste)
---
+++ utils/cnf_to_list_and_set.py
@@ -0,0 +1,17 @@
+def cnf_to_list(archive):
+    with open(f'{archive}', "r") as arquivo:
+        lines = arquivo.readlines()
+        clauses = []
+        for line in lines:
+            if line[0] not in 'cp':
+                line.replace('\n', '')
+                line = line[:-2]
+                clauses.append(line)
+
+        for pos, clause in enumerate(clauses):
+            clause = clause.split()
+            clauses.pop(pos)
+            clauses.insert(pos, set())
+            for item in clause:
+                clauses[pos].add(int(item))
+    return clauses
